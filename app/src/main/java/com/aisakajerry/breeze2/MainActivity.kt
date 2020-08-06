11d19a77d38e10787336cf86bf3c3178679b7e59
AisakaJerry <fengzeyuanasd@gmail.com>
2020-08-06 15:24:51 -0400

    Add play_interface.xml; Bind saerchActivity to searchButton

--- app/src/main/java/com/aisakajerry/breeze2/MainActivity.kt
+++ app/src/main/java/com/aisakajerry/breeze2/MainActivity.kt
@@ -1,25 +1,58 @@
 package com.aisakajerry.breeze2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
 import okhttp3.*
+import okhttp3.HttpUrl
 import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     private val client = OkHttpClient()
+    private val moshi = Moshi.Builder().build()
+    private val gistJsonAdapter = moshi.adapter(Gist::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        run("http://139.196.190.189/search")
+        val button: Button = findViewById(R.id.searchButton)
+        button.setOnClickListener {
+            runSearch()
+        }
+        //runSearch()
     }
-    fun run(url: String) {
+    fun runSearch() {
+        println("Start to fetch URL json")
+
+        val url = HttpUrl.Builder()
+            .scheme("https")
+            .host("www.google.com")
+            .addPathSegment("search")
+            .addQueryParameter("q", "fly me to the moon")
+            .build()
         val request = Request.Builder()
             .url(url)
             .build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: Response) {
+                val gist = gistJsonAdapter.fromJson(response.body!!.source())
+
+                for ((key, value) in gist!!.files!!) {
+                    println(key)
+                    println(value.content)
+                }
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Fail to execute request")
+            }
         })
     }
+    @JsonClass(generateAdapter = true)
+    data class Gist(var files: Map<String, GistFile>?)
+
+    @JsonClass(generateAdapter = true)
+    data class GistFile(var content: String?)
 }
